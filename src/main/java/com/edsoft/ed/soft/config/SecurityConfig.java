191448deabf97fb75ad61ff1ac3272776d7aabc6
erdincdagli35 <3rd1fbslash@gmail.com>
2026-04-11 08:19:27 +0300

    securityConfig has been added v.2

--- src/main/java/com/edsoft/ed/soft/config/SecurityConfig.java
+++ src/main/java/com/edsoft/ed/soft/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.edsoft.ed.soft.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.*;
 import org.springframework.web.filter.CorsFilter;
 
@@ -21,5 +23,17 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", config);
 
         return new CorsFilter(source);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf().disable()
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+        return http.build();
     }
 }
